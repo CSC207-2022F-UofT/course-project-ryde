eee3ad0bfa2ad92ec36dad874c01208d18105b5b
Ananya Jain <ananya.jain@mail.utoronto.ca>
2022-11-28 17:18:21 -0500

    Finished writing javadocs for the display listings use case!

--- src/main/java/intefaceAdapters/displayListing/DisplayListingScreenInterface.java
+++ src/main/java/intefaceAdapters/displayListing/DisplayListingScreenInterface.java
@@ -5,7 +5,13 @@ import useCases.displayListing.DisplayListingDsRequestModel;
 import java.util.List;
 
 public interface DisplayListingScreenInterface {
+    /**
+     * @param filteredListings filtered listings
+     */
     void displayListing(List<DisplayListingDsRequestModel> filteredListings);
 
+    /**
+     * @param message The failure message
+     */
     void displayMessage(String message);
 }
--- src/main/java/useCases/displayListing/DisplayListingInteractor.java
+++ src/main/java/useCases/displayListing/DisplayListingInteractor.java
@@ -4,15 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * This is DisplayListingUseCase.
+ */
 public class DisplayListingInteractor implements DisplayListingInputBoundary{
     private final DisplayListingDsGateway gateway;
     private final DisplayListingPresenter presenter;
 
+    /**
+     * @param presenter response formatter for the use case.
+     * @param gateway meant to be the DisplayListingRepo.
+     */
     public DisplayListingInteractor(DisplayListingPresenter presenter, DisplayListingDsGateway gateway) {
         this.presenter = presenter;
         this.gateway = gateway;
     }
 
+    /**
+     * @param displayListingRequestModel stores the filter input by the user.
+     * tells the presenter to display listings if found else display the failure message.
+     */
     @Override
     public void displayListings(DisplayListingRequestModel displayListingRequestModel) {
         List<DisplayListingDsRequestModel> listings = gateway.getListings();
